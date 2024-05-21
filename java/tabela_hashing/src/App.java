1a0586d81e8bab85f41dd750ad8c62155b5bd0d2
Thiago Marcolino <tmarcolino40@gmail.com>
2024-05-20 21:23:39 -0300

    modificado

--- java/tabela_hashing/src/App.java
+++ java/tabela_hashing/src/App.java
@@ -1,7 +1,10 @@
 import java.util.LinkedList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class App {
     static LinkedList tabela[] = new LinkedList[26];
+    static Scanner input = new Scanner(System.in);
     public static int hash(String valor){
         valor = valor.toLowerCase();
         int codigoAscii = valor.charAt(0);
@@ -16,18 +19,76 @@ public class App {
         }
         tabela[categoria].add(valor);
         
-    }
-
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        adiciona("astrogildo");
-        adiciona("Silvio Santos");
-        adiciona("anibal");
-        adiciona("creonilso");
-        // imprimir todos
-        for( LinkedList lista: tabela){
-            System.out.println(lista);
-        }
         
+    }
+        public static void imprimir(){
+            System.out.println("-----------------");
+            for (int i = 0; i < tabela.length; i++) {
+                LinkedList lista = tabela[i];
+                if (lista!= null){
+                    int total = lista.size();
+                    System.out.println((i+"Total: " + total + "->" + lista));
+                }else{
+                    System.out.println(i + "Total: " + 0 + "->" + lista);
+                }
+                
+                
+            }
+        }
+
+      
+
+    
+        public static void gerarNomes(int qtdPalavra){
+            Random rnd = new Random();
+            for (int i = 0; i < qtdPalavra; i++) {
+                int qtdLetras = rnd.nextInt(3,9);
+                String palavra = "";
+                for (int j = 0; j < qtdLetras; j++) {
+                    char letra = (char)rnd.nextInt(97,123);
+                    palavra += letra;
+                }
+                adiciona(palavra);
+                
+            }
+            
+        }
+
+        public static int menu(){
+            
+            System.out.println("***MENU***");
+            System.out.println("1 - Buscar");
+            System.out.println("2 - Sair");
+            System.out.println(">>>>");
+            return input.nextInt();
+        }
+
+        public static void busca(String palavra){
+            int categoria = hash(palavra);
+            System.out.println("***BUSCA***");
+            System.out.println("categoria " + categoria);
+            LinkedList lista = tabela[categoria];
+            if (lista.contains(palavra)){
+                System.out.println(palavra + " Esta presente");
+
+            }else{
+                System.out.println(palavra + " Não está presente");
+            }
+        }
+
+
+    public static void main(String[] args){
+        gerarNomes(100);
+        imprimir();
+        int op = 0;
+        do {
+            op=menu();
+            if (op ==1){
+                System.out.println("Digite a palavra a ser procurada");
+                String palavra = input.next();
+                busca(palavra);
+
+            }
+        } while (op!=2);
     }
 }
